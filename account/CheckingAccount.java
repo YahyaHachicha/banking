2c086e09f7a1ac6540a7d8c44b44316119769cad
Yahya Hachicha <101501346+YahyaHachicha@users.noreply.github.com>
2025-12-25 20:37:11 +0100

    Complete CheckingAccount class methods and fields.
    Complete all CheckingAccount class methods and fields.

--- account/CheckingAccount.java
+++ account/CheckingAccount.java
@@ -8,6 +8,9 @@ package banking.account;
  */
 public class CheckingAccount implements BankAccount {
     // TODO:
+    private int accountNumber, pin;
+    private Customer owner;
+    private double balance;
 
     /**
      * Creates a new checking bank account for the given customer.
@@ -18,56 +21,69 @@ public class CheckingAccount implements BankAccount {
      */
     public CheckingAccount(int accountNumber, Customer owner, int pin) {
         // TODO:
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.pin = pin;
     }
 
     @Override
     public final AccountType getAccountType() {
         // TODO:
-        return null;
+        return AccountType.Checking;
     }
 
     @Override
     public final void closeAccount() {
         // TODO:
+        AccountType closing = AccountType.Savings;
     }
 
     @Override
     public boolean validatePin(int pin) {
         // TODO:
+        if (this.pin == pin) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public Customer getOwner() {
         // TODO:
-        return null;
+        return this.owner;
     }
 
     @Override
     public int getAccountNumber() {
         // TODO:
-        return 0;
+        return this.accountNumber;
     }
 
     @Override
     public double getBalance() {
         // TODO:
-        return 0;
+        return this.balance;
     }
 
     @Override
     public void withdrawMoney(double money) {
         // TODO:
+        if (money > 0 && money <= this.balance) {
+            this.balance -= money;
+        }
     }
 
     @Override
     public void depositMoney(double money) {
         // TODO:
+        if (money > 0){
+            this.balance += money;}
     }
 
     @Override
     public final String getAccountInformation(){
         // TODO:
-        return null;
+        String accountInformation = "Owner: " + this.owner + ", AccountNumber: " + this.accountNumber + ", Pin: " + this.pin + ", Balance: " + this.balance;
+        return accountInformation;
     }
 }
