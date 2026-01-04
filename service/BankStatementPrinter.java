462b0435345ef8f79c791388b5d69e3dfb87ff08
Hunter <167322676+Hunterthehh@users.noreply.github.com>
2026-01-04 14:26:58 +0100

    finished BankStatementPrinter... kinda...
    the class now implements all code asked of by the assignment but i dont think its written that nice because i had some problems using the already written code by amir.
    i will probably look into this later on but it should work for now.

--- service/BankStatementPrinter.java
+++ service/BankStatementPrinter.java
@@ -12,17 +12,44 @@ import java.util.Scanner;
  */
 public class BankStatementPrinter {
     public static boolean printStatement(Bank bank) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
         System.out.println("**********************************************************");
-        System.out.println("Welcome to "+bank.getInstitutionName());
+        System.out.println("Welcome to " + bank.getInstitutionName());
         System.out.println("**********************************************************");
         System.out.println("Please enter your account number:");
 
-        return printStatementOf(bank, scanner.nextInt());
+        return printStatementOf(bank, scanner1.nextInt());
     }
 
     public static boolean printStatementOf(Bank bank, int accountNumber) {
-        // TODO:
+        if (bank.getBankAccount(accountNumber) != null) { //getbankaccount muss durch alle bankaccounts der institution laufen
+            int triesLeft = 4;
+            Scanner scanner2 = new Scanner(System.in);
+
+            System.out.println("**********************************************************");
+            System.out.print("Account found. \n Please enter your pin: ");
+            for (int tries = 4; tries > 0; tries--) {
+                if (bank.getBankAccount(accountNumber).validatePin(scanner2.nextInt())) {
+                    System.out.println();
+                    if (bank.getBankAccount(accountNumber).accessibleFromTerminal()) {
+                        System.out.println("Your balance for this account is: " + bank.getBankAccount(accountNumber).getBalance());
+                        return true;
+                    } else {
+                        System.out.println("Insufficient access to terminal. Process aborted");
+                        return false;
+                    }
+                } else {
+                    triesLeft -= 1;
+                    System.out.println();
+                    System.out.println("Wrong pin. Try again.");
+                    System.out.println("Tries left: " + triesLeft);
+                    System.out.println("Tries left: " + triesLeft);
+                    System.out.print("Please enter your pin: ");
+                }
+            }
+            System.out.println("No tries left. Process aborted.");
+            return false;
+        }
         return false;
     }
 }
