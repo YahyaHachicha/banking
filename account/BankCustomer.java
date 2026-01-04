dfc87efab022a05afec4507613843d6046d3a25d
Hunter <167322676+Hunterthehh@users.noreply.github.com>
2026-01-04 14:24:03 +0100

    fixed a syntax error
    there was a semicolon unnecessary semicolon breaking the code.

--- account/BankCustomer.java
+++ account/BankCustomer.java
@@ -32,7 +32,7 @@ public class BankCustomer implements Customer {
 
     public int getCustomerNumber() {
         // TODO:
-        return customerNumber;;
+        return customerNumber;
     }
 
     /**
